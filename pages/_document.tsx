215589bca1a94e0828ab7593b74c384f9471763f
Kegen Guyll <guyllkegen@gmail.com>
2020-12-31 15:44:59 -0600

    Created meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Dad Jokes</title>
+          <meta name='title' content='Dad Jokes' />
+          <meta name='description' content='One of the best Dad Jokes APIs' />
+
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://dadjokes.io' />
+          <meta property='og:title' content='Dad Jokes' />
+          <meta
+            property='og:description'
+            content='One of the best Dad Jokes APIs'
+          />
+          <meta property='og:image' content='/images/DadJokes.png' />
+
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta property='twitter:url' content='https://dadjokes.io' />
+          <meta property='twitter:title' content='Dad Jokes' />
+          <meta
+            property='twitter:description'
+            content='One of the best Dad Jokes APIs'
+          />
+          <meta property='twitter:image' content='/images/DadJokes.png' />
           <link
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap'
             rel='stylesheet'
