2b8eb9d54a72c874cb1492b5ad2d5c01bc5d31b2
Kegen Guyll <guyllkegen@gmail.com>
2021-01-14 15:37:19 -0600

    created search jokes showcase page

---
+++ pages/showcase/search-jokes.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import React from 'react';
+import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import Navigation from '../../components/Navigation';
+import JokeByIdShowcase from '../../components/showcase/JokeById';
+import ShowcaseNavigation from '../../components/showcase/Navigation';
+import RandomJokeShowcase from '../../components/showcase/RandomJoke';
+import SearchJokehowcase from '../../components/showcase/search-jokes';
+
+const Showcase = () => {
+  return (
+    <Container>
+      <Head>
+        <title>DadJokes.io Showcase JokeById</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <nav>
+        <Navigation />
+      </nav>
+      <main>
+        <Container>
+          <ShowcaseNavigation />
+          <SearchJokehowcase />
+        </Container>
+      </main>
+    </Container>
+  );
+};
+
+export default Showcase;
