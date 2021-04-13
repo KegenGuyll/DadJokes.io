ed9060a0dddf392252d287cff04b73cf929348b6
Kegen Guyll <guyllkegen@gmail.com>
2021-04-12 21:30:13 -0500

    added screen res breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: { min: '320px', max: '480px' },
+      md: { min: '481px', max: '768px' },
+      lg: { min: '1025px', max: '1200px' },
+      xl: { min: '1201px' },
+    },
     extend: {},
   },
   variants: {
