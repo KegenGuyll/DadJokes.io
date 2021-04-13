76d0ea8d065146d19036ad5325bbacc04b2bf10f
Kegen Guyll <guyllkegen@gmail.com>
2021-04-12 21:47:24 -0500

    added pagination

---
+++ pages/api/joke/pagination.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { JokeBody } from '../../../models/social/joke';
+
+export default async (req: NextApiRequest, res: NextApiResponse<JokeBody>) => {
+  const skip = req.query.skip as string;
+  const limit = req.query.limit as string;
+  try {
+    const result: AxiosResponse<JokeBody> = await axios.get(
+      `http://localhost:8080/api/joke/pagination?skip=${skip}&limit=${limit}`
+    );
+
+    res.status(200).json(result.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
